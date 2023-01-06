4309458a6965b720058f660e526fbff6112880ad
Dave Matney <matneyx@gmail.com>
2023-01-06 13:33:49 -0700

    Renamed test constants to make more sense

--- src/components/__tests__/BootstrapIcons.test.js
+++ src/components/__tests__/BootstrapIcons.test.js
@@ -37,19 +37,19 @@ describe('MobileNavToggle', () => {
   test('toggles icon when clicked', () => {
     const handleClick = jest.fn();
 
-    const bsList = 'M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5z';
+    const bsListPathDAttributeValue = 'M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5z';
 
-    const bsX = 'M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z'
+    const bsXPathDAttributeValue = 'M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z'
 
     render(<MobileNavToggle onClick={handleClick} />);
-    expect(screen.getByTestId('mobile-nav-toggle').querySelector('path')).toHaveAttribute('d', bsList)
+    expect(screen.getByTestId('mobile-nav-toggle').querySelector('path')).toHaveAttribute('d', bsListPathDAttributeValue)
 
     fireEvent.click(screen.getByTestId('mobile-nav-toggle'));
 
-    expect(screen.getByTestId('mobile-nav-toggle').querySelector('path')).toHaveAttribute('d', bsX)
+    expect(screen.getByTestId('mobile-nav-toggle').querySelector('path')).toHaveAttribute('d', bsXPathDAttributeValue)
 
     fireEvent.click(screen.getByTestId('mobile-nav-toggle'));
 
-    expect(screen.getByTestId('mobile-nav-toggle').querySelector('path')).toHaveAttribute('d', bsList)
+    expect(screen.getByTestId('mobile-nav-toggle').querySelector('path')).toHaveAttribute('d', bsListPathDAttributeValue)
   })
 });
